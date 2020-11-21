40efcfcd2a5036aa73ad23220596a5b44489ae2c
Itay Dumay <69404263+Itay-Dum@users.noreply.github.com>
2020-11-21 23:38:49 +0200

    Update client.py

--- ClientFiles/client.py
+++ ClientFiles/client.py
@@ -1,31 +1,62 @@
 import socket
 import pickle
-from cryptography.fernet import Fernet
-from PyQt5.QtWidgets import QMessageBox
+from cryptography.fernet import Fernet, InvalidToken
 import os
-from color import Color
 
-IP = "127.0.0.1"
-PORT = 6969
+PyQt_import = True
+try:
+    from PyQt5.QtWidgets import QMessageBox
+except (ImportError, QMessageBox):
+    PyQt_import = False
+
+IP = "127.0.0.1"  # The server's IP, blank to use the computers' public ip.
+PORT = 6969  # General port is 6969, to upload files it's 6970 and to download files it's 6968
 ADDR = (IP, PORT)
 
-color = Color()
+
+class DetailsNotCorrect(Exception):
+    pass
 
 
 class ServerNotRunning(Exception):
     pass
 
 
+#  Make the output to the console be in colors.
+class Color(object):
+    def __init__(self):
+        self.PURPLE = '\033[1;35;48m'
+        self.CYAN = '\033[1;36;48m'
+        self.BOLD = '\033[1;37;48m'
+        self.BLUE = '\033[1;34;48m'
+        self.GREEN = '\033[1;32;48m'
+        self.YELLOW = '\033[1;33;48m'
+        self.RED = '\033[1;31;48m'
+        self.BLACK = '\033[1;30;48m'
+        self.UNDERLINE = '\033[4;37;48m'
+        self.END = '\033[1;37;0m'
+
+    def red(self, string):
+        return f"{self.RED}{string}{self.END}"
+
+    def green(self, string):
+        return f"{self.GREEN}{string}{self.END}"
+
+
+color = Color()
 
 
 class Client(object):
+    # Client class, responsible for logging to account, signing up to a new account, list_files method,
     def __init__(self):
         self.__sock = socket.socket()
         try:
             self.__sock.connect(ADDR)
             self.__key = Fernet(self.__sock.recv(1024))
-        except WindowsError or Exception:
-            if __name__ == "__main__":
+            self.__username = None
+            self.__password = None
+        except (WindowsError, InvalidToken):
+            if PyQt_import:
                 msg = QMessageBox()
                 msg.setIcon(QMessageBox.Critical)
                 msg.setText("The Server is not running currently, please try again later!")
@@ -33,17 +64,19 @@ class Client(object):
                 msg.exec_()
                 raise ServerNotRunning()
             else:
-                print(color.red(f"The Server is not running currently, please try again later!"))
-                exit(1)
+                raise ServerNotRunning(
+                    "The Server is not running currently, btw you don't have PyQt5 installed, use \"pip install PYQt5\" to use the GUI version.")
 
     def login(self, username, password):
         self.__sock.send(self.__key.encrypt(b"login"))
         self.__sock.send(self.__key.encrypt(pickle.dumps({"username": username, "password": password})))
         response = pickle.loads(self.__key.decrypt(self.__sock.recv(1024)))
         if response:
+            self.__username = username
+            self.__password = password
             print(color.green(f"You have been successfully connected, {username}"))
         else:
-            print(color.red("The details you entered are incorrect unable to login"))
+            raise DetailsNotCorrect(color.red("The details you entered are incorrect unable to login"))
         return response
 
     def sign_up(self, username, password, email):
@@ -52,53 +85,64 @@ class Client(object):
         response = pickle.loads(self.__key.decrypt(self.__sock.recv(1024)))
         return response
 
-    def delete_user(self, username, password):
+    def delete_user(self):
         self.__sock.send(self.__key.encrypt(b"delete_user"))
-        self.__sock.send(self.__key.encrypt(pickle.dumps({"username": username, "password": password})))
+        self.__sock.send(self.__key.encrypt(pickle.dumps({"username": self.__username, "password": self.__password})))
 
-    def upload(self, path, username):
-        try:
-            file_name = os.path.basename(path)
-            file_size = os.path.getsize(path)
-            self.__sock.send(self.__key.encrypt(b"upload_file"))
-            self.__sock.send(self.__key.encrypt(pickle.dumps({"username": username, "file_name": file_name, "file_size": file_size})))
-            with open(path, "rb") as file:
-                temp = file.read(2048)
-                while temp:
-                    self.__sock.send(self.__key.encrypt(temp))
-                    temp = file.read(2048)
-            return True
-        except Exception or FileNotFoundError:
-            return False
+    def upload(self, path):
+        if isinstance(path, list):
+            for p in path:
+                UploadFiles(self.__username, self.__password, p)
+        else:
+            UploadFiles(self.__username, self.__password, path)
 
-    def upload_file1(self, path, username):
-        file_name = os.path.basename(path)
-        self.__sock.send(self.__key.encrypt(b"upload_file"))
-        self.__sock.send(self.__key.encrypt(pickle.dumps({"username": username, "file_name": file_name})))
-        with open(path, "rb") as file:
-            temp = file.read(2048)
-            while temp:
-                self.__sock.send(self.__key.encrypt(temp))
-                temp = file.read(2048)
-            self.__sock.send(self.__key.encrypt(b"endfile"))
+    def get_files(self):
+        self.__sock.send(self.__key.encrypt(b"get_files"))
+        files = pickle.loads(self.__key.decrypt(self.__sock.recv(4096)))
+        print(files)
+        return files
 
-    def upload_file(self, path, username):
-        file_name = os.path.basename(path)
 
-        with open(path, "rb") as file:
+class UploadFiles(object):
+    def __init__(self, username, password, path):
+        self.IP = IP
+        self.PORT = PORT + 1
+        self.path = path
+        self.__sock = socket.socket()
+        self.__sock.connect((self.IP, self.PORT))
+        self.__username = username
+        self.__password = password
+        self.__key: (Fernet, None) = None
+        self.verify_details()
+
+    def verify_details(self):
+        self.__key = Fernet(self.__sock.recv(1024))
+        self.__sock.send(self.__key.encrypt(pickle.dumps({"username": self.__username, "password": self.__password})))
+        response: bool = pickle.loads(self.__key.decrypt(self.__sock.recv(1024)))
+        if response:
+            self.upload()
+        else:
+            raise DetailsNotCorrect()
+
+    def upload(self):
+        base_name = os.path.basename(self.path)  # e.g c:/user/Desktop/pic.png -> pic.png
+        UploadFiles.copyfile(self.__key, self.path)
+        self.__sock.send(self.__key.encrypt(base_name.encode()))
+        with open(f"{self.path}.copy", "rb") as file:
             file_data = file.read(2048)
             while file_data:
-                self.__sock.send(self.__key.encrypt(f"file {file_name} ".encode() + file_data))
+                self.__sock.send(file_data)
                 file_data = file.read(2048)
-        self.__sock.send(self.__key.encrypt(f"endfile {file_name}".encode()))
+        print("Closing the connection, file uploaded")
+        os.remove(f"{self.path}.copy")
+        self.__sock.close()
+
+    @staticmethod
+    def copyfile(key, path):
+        with open(path, "rb") as origin:
+            with open(f"{path}.copy", "wb") as file:
+                file.write(key.encrypt(origin.read()))
 
 
 if __name__ == '__main__':
-    client = Client()
-    client.login("itayd", "i")
-    client.upload_file("test.py", "itay")
-
-
-
-
-
+    pass
